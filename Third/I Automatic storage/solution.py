d8f24de689b0280591a592b8fd61735cb283fc31
n.nikiforov <nsnikiforov@spbaep.ru>
2024-11-08 16:06:02 +0300

    To sync

--- Third/I Automatic storage/solution.py
+++ Third/I Automatic storage/solution.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Solution:
 
     @staticmethod
@@ -5,42 +8,75 @@ class Solution:
         with open("input.txt", "r") as f:
             raw = f.readlines()
             n = int(raw[0])
-            interest_ranks = list(map(int, raw[1].split()))
-            useful_ranks = list(map(int, raw[2].split()))
-            mood = list(map(int, raw[3].split()))
-        return n, interest_ranks, useful_ranks, mood
+            a, b = list(map(int, raw[1].split()))
+            rovers = raw[2:]
+        return n, a, b, rovers
 
-    @staticmethod
-    def solve(n: int, interest_ranks: list[int], useful_ranks: list[int], mood: list[int]) -> list[int]:
-        used_indexes = set()
+    def solve(self, n: int, a, b, rovers) -> list[int]:
         result = []
-        zipped = list(zip(range(1, n + 1), interest_ranks, useful_ranks))
-        interest_counter = 0
-        useful_counter = 0
-        ranged_by_interest = sorted(zipped, key=lambda x: (x[1], x[2], -x[0]), reverse=True)
-        ranged_by_useful = sorted(zipped, key=lambda x: (x[2], x[1], -x[0]), reverse=True)
-
-        for m in mood:
-            if not m:
-                while (ranged_by_interest[interest_counter][0] in used_indexes
-                       and interest_counter < len(ranged_by_interest) - 1):
-                    interest_counter += 1
-                index = ranged_by_interest[interest_counter][0]
-                result.append(index)
-                used_indexes.add(index)
-                interest_counter += 1
-            else:
-                while (ranged_by_useful[useful_counter][0] in used_indexes
-                    and useful_counter < len(ranged_by_useful) - 1):
-                    useful_counter += 1
-                index = ranged_by_useful[useful_counter][0]
-                result.append(index)
-                used_indexes.add(index)
-                useful_counter += 1
+        priorities = [0] * 4
+        priorities[a - 1] = 1
+        priorities[b - 1] = 1
+        rovers = [tuple(map(int, x.split())) for x in rovers]
+        rovers = list(zip(range(1, len(rovers) + 1), rovers))
+        rovers = sorted(rovers, key=lambda x: (x[1][1], x[1][0]))
+        max_time = rovers[-1][1][1]
+        times = [None] * max_time
+        for rover in rovers:
+            time = rover[1][1] - 1
+            side = rover[1][0] - 1
+            if times[time] is None:
+                times[time] = [0] * 4
+            times[time][side] = rover[0]
+        for i, time in enumerate(times):
+            if time is None:
+                times[i] = [0] * 4
+        cross = [
+            deque(),
+            deque(),
+            deque(),
+            deque(),
+        ]
+        this_time = 0
+        while cross[0] or cross[1] or cross[2] or cross[3] or this_time < len(times):
+            if this_time < len(times):
+                time = times[this_time]
+                for side, index in enumerate(time):
+                    if index:
+                        cross[side].appendleft(index)
+            cross_state = [0] * 4
+            for i, side in enumerate(cross):
+                if side and side[-1]:
+                    cross_state[i] = 1
+            go_list = [0] * 4
+            for side in range(4):
+                if self.can_go(priorities, side, cross_state):
+                    go_list[side] = 1
+            for side, can_go in enumerate(go_list):
+                if can_go and cross[side]:
+                    result.append((this_time + 1, cross[side].pop()))
+            this_time += 1
+        result = sorted(result, key=lambda x: x[1])
+        result = [x[0] for x in result]
         return result
 
     def solve_from_input(self):
-        print(*self.solve(*self.get_input()))
+        result = self.solve(*self.get_input())
+        for item in result:
+            print(item)
+
+    @staticmethod
+    def can_go(priorities, side, cross_state):
+        if priorities[side]:
+            if not (priorities[side - 1] and cross_state[side - 1]):
+                return True
+            return False
+        else:
+            for i, priority in enumerate(priorities):
+                if priority and cross_state[i]:
+                    return False
+            if not (priorities[side - 1] == 0 and cross_state[side - 1]):
+                return True
 
 
 if __name__ == "__main__":
--- Third/J Bed out of chairs/solution.py
+++ Third/J Bed out of chairs/solution.py
@@ -1,40 +1,43 @@
+from collections import deque
+
+
 class Solution:
 
     @staticmethod
     def get_input():
         with open("input.txt", "r") as f:
             raw = f.readlines()
-            n = int(raw[0])
-            weights = list(map(int, raw[1].split()))
-            m, k = list(map(int, raw[2].split()))
-            evidence_numbers = list(map(int, raw[3].split()))
-        return n, weights, m, k, evidence_numbers
+            n, h = list(map(int, raw[0].split()))
+            heights = list(map(int, raw[1].split()))
+            widths = list(map(int, raw[2].split()))
+        return n, h, heights, widths
 
     @staticmethod
-    def solve(n, weights, m, k, evidence_numbers) -> list[int]:
-        result = []
-        eq_prefixes = [0]
-        for i in range(1, len(weights)):
-            if weights[i] == weights[i - 1]:
-                eq_prefixes.append(eq_prefixes[i - 1] + 1)
-            else:
-                eq_prefixes.append(eq_prefixes[i - 1])
-        steps_map = []
-        j = 0
-        prev_w = weights[j]
-        for i in range(len(eq_prefixes)):
-            while j < len(eq_prefixes) and eq_prefixes[j] - eq_prefixes[i] <= k and prev_w <= weights[j]:
-                steps_map.append(i + 1)
-                prev_w = weights[j]
-                j += 1
-            if j == i + 1:
-                prev_w = weights[min(j, len(weights) - 1)]
-        for evidence in evidence_numbers:
-            result.append(steps_map[evidence - 1])
-        return result
+    def solve(n, h, heights, widths) -> int:
+        zipped = list(zip(heights, widths))
+        zipped = sorted(zipped, key=lambda x: x[0])
+        dq = deque()
+        this_width = 0
+        i = 0
+        max_diff = None
+        prev_h = None
+        for chair_height, chair_width in zipped:
+            this_width += chair_width
+            dq.append((chair_height, chair_width))
+            if this_width >= h:
+                if prev_h:
+                    if max_diff is None:
+                        max_diff = chair_height - prev_h
+                    else:
+                        max_diff = max(max_diff, chair_height - prev_h)
+                chair_out = dq.popleft()
+                this_width -= chair_out[1]
+            prev_h = chair_height
+
+        return max_diff
 
     def solve_from_input(self):
-        print(*self.solve(*self.get_input()))
+        print(self.solve(*self.get_input()))
 
 
 if __name__ == "__main__":
